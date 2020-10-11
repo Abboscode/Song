24d2936bb86f5c97ef2d29bd519c47d02cebea89
Abboscode <60695374+Abboscode@users.noreply.github.com>
2020-10-11 15:39:45 +0500

    Add files via upload
    pirple.com ex.

---
+++ Song.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>) {
+
+/*this project aims to bring up all the information about a given song(Wishing Well)*/
+    val songName = "Wishing well"
+    val author: String = "Jucy WRLD"
+    val realiseDate: String = "July 28,2020"
+    val rating: Double = 9.2
+    val billboardRanking: Int = 5
+    val nominated: String? = null
+    val genre = "Soft Rock"
+    val album: String = "Legends never die"
+    val youTubeViews: Int = 71000000
+    val producer: String = "Dr. Luke"
+    val recorded: Int = 2019
+    val label = "Grade A"
+    val songWriter = "Jarad Higins"
+    val length: String = "3:14"
+    val australia = 14
+    val canada = 8
+    val usBillboardHot = 4
+
+    println("Song name: $songName")
+    println("Author: $author")
+    println("Realise date: $realiseDate")
+    println("Rate: $rating")
+    println("Billboard ranking $billboardRanking")
+    println("Nomination: $nominated" )
+
+    println("Genre: $genre")
+    println("Album: $album")
+    println("Views on YouTube: $youTubeViews")
+    println("Producer: $producer")
+    println("Record date: $recorded")
+    println("Label: $label")
+    println("Song Writer: $songWriter")
+    println("Length: $length")
+    println("Chart Australia No: $australia")
+    println("Chart Canada No: $canada")
+    println("Chart US \"BILLBOARD HOT\"No: $usBillboardHot")
+
+}
